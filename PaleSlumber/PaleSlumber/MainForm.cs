b84d766fdffa3e168f418ca18866a510e45409ed
popo <wkoidect@gmail.com>
2025-05-27 08:00:31 +0900

    プロジェクト作成

---
+++ PaleSlumber/PaleSlumber/MainForm.cs
@@ -0,0 +1,20 @@
+namespace PaleSlumber
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
